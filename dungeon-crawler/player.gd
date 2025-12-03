6d48be6c7de73cef0f7828a42447afe518a32dfa
Linus Horn <linus@linush.org>
2025-12-03 18:23:13 +0100

    working charachter direction

--- dungeon-crawler/player.gd
+++ dungeon-crawler/player.gd
@@ -3,12 +3,23 @@ extends CharacterBody2D
 const SPEED = 200.0
 const TILE_DOOR = 3
 
+
 func _physics_process(_delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	if direction:
 		velocity = direction * SPEED
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, SPEED)
+	
+	# Change animation Frame from child node Sprite2D, according to movement direction
+	if direction.x > 0:
+		$Sprite2D.frame = 2  # Right
+	elif direction.x < 0:
+		$Sprite2D.frame = 3
+	elif direction.y < 0:
+		$Sprite2D.frame = 1
+	elif direction.y > 0:
+		$Sprite2D.frame = 0  # Downwds
 
 	move_and_slide()
 
